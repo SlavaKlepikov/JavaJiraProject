8699a274399aea3832f0908157592a9cd7da00bb
Slava_Klepikov <SlavaKlepikov@bigmir.net>
2018-09-08 23:05:12 +0300

    removed static getName  method

--- src/main/java/ITestListenerClass.java
+++ src/main/java/ITestListenerClass.java
@@ -6,27 +6,27 @@ import org.testng.ITestResult;
 public class ITestListenerClass implements ITestListener {
     @Override
     public void onTestStart(ITestResult iTestResult) {
-        System.out.println("onTestStart: " +  getTestMethodName(iTestResult) + " start");
+        System.out.println("onTestStart: " +  iTestResult.getName() + " start");
     }
 
     @Override
     public void onTestSuccess(ITestResult iTestResult) {
-        System.out.println("onTestSuccess: " +  getTestMethodName(iTestResult) + " succeed");
+        System.out.println("onTestSuccess: " +  iTestResult.getName() + " succeed");
     }
 
     @Override
     public void onTestFailure(ITestResult iTestResult) {
-        System.out.println("onTestFailure: " +  getTestMethodName(iTestResult) + " failed");
+        System.out.println("onTestFailure: " +  iTestResult.getName() + " failed");
     }
 
     @Override
     public void onTestSkipped(ITestResult iTestResult) {
-        System.out.println("onTestSkipped: "+  getTestMethodName(iTestResult) + " skipped");
+        System.out.println("onTestSkipped: "+  iTestResult.getName() + " skipped");
     }
 
     @Override
     public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
-        System.out.println("onTestFailedButWithinSuccessPercentage: " + getTestMethodName(iTestResult));
+        System.out.println("onTestFailedButWithinSuccessPercentage: " + iTestResult.getName());
     }
 
     @Override
@@ -38,10 +38,5 @@ public class ITestListenerClass implements ITestListener {
     public void onFinish(ITestContext iTestContext) {
         System.out.println("onFinish: " + iTestContext.getName());
     }
-
-    private static String getTestMethodName(ITestResult iTestResult) {
-        return iTestResult.getMethod().getConstructorOrMethod().getName();
-    }
-
 
 }
